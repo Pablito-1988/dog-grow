4cbbf4205905b867a4a3249b290e6d273408027e
Pablo Armando <pabloar24.7@hmail.com>
2022-06-18 21:10:03 -0300

    first push!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Main from "./components/Main/Main"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
+import Edu from "./components/Edu/Edu"
+import SellPoint from "./components/SellPoints/SellPoints"
+import About from "./components/About/About"
+import Contact from "./components/Contact/Contact"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="AppContainer" >
+      <p>Hola</p>
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,9 @@
+
+const About = () => {
+    return (
+        <>
+        <p>Hola soy About</p>
+        </>
+    )
+}
+export default About;
