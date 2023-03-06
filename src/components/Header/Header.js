7a1d0769bd4173bd5287ebf9531eda1253768fdc
Pablo Armando <pabloar24.7@hmail.com>
2023-03-06 16:21:47 -0300

    header responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,94 @@
-import React from "react";
-import "./Header.css";
-import logo from "../../assets/images/DogPNG.png";
-import { useState, useRef } from "react";
-import Dropdown from "./Dropdown";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './Header.css';
+import logo from '../../assets/images/DogPNG.png';
+import { useState, useRef } from 'react';
+import Dropdown from './Dropdown';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [DropdownMenu, setDropdown] = useState(false);
   const dropdownRef = useRef(null);
   const handleDropdown = () => {
     setDropdown(!DropdownMenu);
-    if(DropdownMenu === true) {
-    dropdownRef.current.style.transform = "rotate(0deg)";
-    }else{
-      dropdownRef.current.style.transform = "rotate(180deg)";
+    if (DropdownMenu === true) {
+      dropdownRef.current.style.transform = 'rotate(0deg)';
+    } else {
+      dropdownRef.current.style.transform = 'rotate(180deg)';
     }
   };
 
   return (
     <>
       <header>
-        <div className="headerContainer">
-          <div className="iconContainer">
-            <i
-              onClick={handleDropdown}
-              id="burgerMenu"
-              className="fas fa-bars"
-            ></i>
-            <i
-              ref={dropdownRef}
-              id="arrowMenu"
-              className="fas fa-angle-down"
-            ></i>
+        <div className='mobileHeader'>
+          <div className='headerContainer'>
+            <div className='iconContainer'>
+              <i
+                onClick={handleDropdown}
+                id='burgerMenu'
+                className='fas fa-bars'
+              ></i>
+              <i
+                ref={dropdownRef}
+                id='arrowMenu'
+                className='fas fa-angle-down'
+              ></i>
+            </div>
+          </div>
+          <div className='headerLogo'>
+            <Link to={'/'}>
+              <img
+                className='logoImg'
+                src={logo}
+                alt='logo'
+              />
+            </Link>
+          </div>
+          <div className='headerSpace'></div>
+        </div>
+        <div className='desktopHeader'>
+          <div className='headerLogo'>
+            <Link to={'/'}>
+              <img
+                className='logoImg'
+                src={logo}
+                alt='logo'
+              />
+            </Link>
+          </div>
+          <div className='desktopLinks'>
+            <Link
+              className='navLink'
+              to='/catalogo'
+            >
+              CATEGORIAS
+            </Link>
+            <Link
+              className='navLink'
+              to='/edu'
+            >
+              EDU
+            </Link>
+            <Link
+              className='navLink'
+              to='/sell-points'
+            >
+              PUNTOS DE VENTA
+            </Link>
+            <Link
+              className='navLink'
+              to='/contact'
+            >
+              CONTACTO
+            </Link>
+            <Link
+              className='navLink'
+              to='/about'
+            >
+              NOSOTROS
+            </Link>
           </div>
         </div>
-        <div className="headerLogo">
-          <Link to={"/"}>
-            <img className="logoImg" src={logo} alt="logo" />
-          </Link>
-        </div>
-        <div className="headerSpace"></div>
       </header>
       {DropdownMenu ? <Dropdown change={handleDropdown} /> : null}
     </>
