70019b808c0eba29783dc541c95390090d2ebd17
Pablo Armando <pabloar24.7@hmail.com>
2023-03-06 16:33:53 -0300

    category responsive

--- src/components/Products/Categories.js
+++ src/components/Products/Categories.js
@@ -45,10 +45,13 @@ const Categories = () => {
     return (
         <div className='categoriesContainer'>
             <h1 className='categoriesMainTitle'>CATEGORIAS</h1>
-            {page && page.map(category => {
+            <div className='categoriesWrapper'>
+              {page && page.map(category => {
                 return (<Category key={category.categoryName} nombre={category.categoryName} imagen={category.categoryImage.url} />)
             }
             )}
+            </div>
+            
         </div>
     );
 };
